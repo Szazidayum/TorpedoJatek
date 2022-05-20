5ef480067c9d9d51527c611f4381315f6dd8f7cb
Szazidayum <szabrinamolnar@gmail.com>
2022-05-20 12:53:54 +0200

    hajo kostruktor létrehozása

--- src/main/Hajo.java
+++ src/main/Hajo.java
@@ -1,10 +1,23 @@
 package main;
 
+import java.util.Random;
+
 public class Hajo {
     private int[] pozicio = new int[3];
     
     public String talalat(int poz){
-        return null;
-        
+        String talaltE = "";
+        poz = (int)(Math.random()*5)+1;
+        poz = pozicio[1];
+        int poz2 = pozicio[1]+1;
+        int poz3 = pozicio[1]+2;
+        if (poz==4){
+            talaltE = "talált";
+        }
+        return talaltE;     
+    }
+
+    public Hajo(int[] pozicio) {
+        this.pozicio = pozicio;
     }
 }
--- src/main/TorpedoTeszt.java
+++ src/main/TorpedoTeszt.java
@@ -1,13 +1,12 @@
 package main;
 
 public class TorpedoTeszt {
-    
+    static int poz;
     public static void main(String[] args) {
-       int poz;
        tesztLoves(poz); 
     }
-    public String tesztLoves(int poz){
-        Hajo hajo = new Hajo();
+    public static String tesztLoves(int poz){
+        Hajo hajo = new Hajo([2,3,4]);
         String t = hajo.talalat(4);
         assert t.equals("talált") : "nem jó a találat ellenőrzése";
         return null;
