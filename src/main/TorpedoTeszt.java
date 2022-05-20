1c615e7e0693b671b79697ece4b2a712059a6286
Szazidayum <szabrinamolnar@gmail.com>
2022-05-20 12:22:50 +0200

    torpedo teszt osztály első teszt+assert

--- src/main/TorpedoTeszt.java
+++ src/main/TorpedoTeszt.java
@@ -1,10 +1,15 @@
 package main;
 
 public class TorpedoTeszt {
+    
     public static void main(String[] args) {
-        
+       int poz;
+       tesztLoves(poz); 
     }
     public String tesztLoves(int poz){
+        Hajo hajo = new Hajo();
+        String t = hajo.talalat(4);
+        assert t.equals("talált") : "nem jó a találat ellenőrzése";
         return null;
 
 }
